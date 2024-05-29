0655a41970992db61de31303220a7c6748c4f86f
Phạm Thanh Tùng <tungpt9x2@gmail.com>
2024-05-29 14:05:07 +0700

    update get data task

--- edl_dag.py
+++ edl_dag.py
@@ -7,29 +7,41 @@ from datetime import datetime
 
 args = {
     'owner': 'airflow',
-    'start_date': datetime.now() - timedelta(minutes=10),
+    'start_date': datetime.now() - timedelta(minutes=60),
     'retries': 1,
 }
 
 dag = DAG(
     dag_id='edl-dag',
     default_args=args,
-    schedule_interval=timedelta(minutes=10) # to make this workflow happen every 10 minutes
+    schedule_interval=timedelta(minutes=60) # to make this workflow happen every 10 minutes
 )
 
-def loadExamStatistic():
+def countExamStatistic():
     redis_hook = RedisHook(redis_conn_id="authoring-redis")
     conn = redis_hook.get_conn()
-    data = conn.llen("AuthoringCache:EXAM_STATISTICS:28052024")
-    print(data)
+    data_len = conn.llen("AuthoringCache:EXAM_STATISTICS:28052024")
+    return data_len
+
+def getExamStatistic(ti):
+    data_len = ti.xcom_pull(task_ids='count_data')
+    redis_hook = RedisHook(redis_conn_id="authoring-redis")
+    conn = redis_hook.get_conn()
+    data = conn.lrange("AuthoringCache:EXAM_STATISTICS:28052024", 0 , data_len - 1)
     return data
+
 with dag:
-    load_data = PythonOperator(
-            task_id='load_data',
-            python_callable=loadExamStatistic,
+    count_data = PythonOperator(
+            task_id='count_data',
+            python_callable=countExamStatistic,
     )
 
-    load_data
+    get_data = PythonOperator(
+            task_id='get_data',
+            python_callable=getExamStatistic,
+    )
+
+    count_data >> get_data
 
 if __name__ == "__main__":
     dag.test()
